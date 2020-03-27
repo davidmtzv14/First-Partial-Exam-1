4ddbd3655c1df9f305293ea6f7f0c8f3f1b96456
David Martinez <david.martinez@interfactura.com>
2020-03-27 08:32:55 -0600

    feat(index): Add delete

--- Coding-Challenge-3/js/index.js
+++ Coding-Challenge-3/js/index.js
@@ -6,24 +6,32 @@ function addEvent() {
 
   btn.addEventListener("click", event => {
     event.preventDefault();
-    newResult = '';
-    result.innerHTML = '';
+    newResult = "";
+    result.innerHTML = "";
 
     if (input.value > 0) {
       for (i = 1; i <= input.value; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
-          newResult += `<div class="fizz-buzz-item fizzbuzz"> fizzbuzz </div>`;
+          newResult += `<div id = "${i}" class="fizz-buzz-item fizzbuzz"> fizzbuzz </div>`;
         } else if (i % 3 === 0) {
-          newResult += `<div class="fizz-buzz-item fizz"> fizz</div>`;
+          newResult += `<div id = "${i}" class="fizz-buzz-item fizz"> fizz</div>`;
         } else if (i % 5 === 0) {
-          newResult += `<div class="fizz-buzz-item buzz"> buzz</div>`;
+          newResult += `<div id = "${i}" class="fizz-buzz-item buzz"> buzz</div>`;
         } else {
-          newResult += `<div class="fizz-buzz-item"> ${i}</div>`;
+          newResult += `<div id = "${i}" class="fizz-buzz-item"> ${i}</div>`;
         }
       }
       result.innerHTML = newResult;
     } else {
       console.log("Provide a positive number");
+    }
+  });
+
+  result.addEventListener("click", event => {
+    event.preventDefault();
+    if (event.target.matches('.fizz-buzz-item')) {
+        element = document.getElementById(event.target.id);
+        element.parentNode.removeChild(element);
     }
   });
 }
